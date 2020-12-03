37bf2c1d49f1161f1449d3d8f62190945587de9a
Sean Owens <seangabrielowens@gmail.com>
2020-12-03 00:52:31 -0500

    2020 Day 3 Solution

--- 2020/day3/part1/src/lib.rs
+++ 2020/day3/part1/src/lib.rs
@@ -1,7 +1,17 @@
 pub fn run(args: &Vec<String>) -> Result<String, &'static str> {
-    for arg in args {
-        println!("{}", arg);
-    }
-    Err("Not Implemented!")
-}
+    let trees = args[0]
+        .trim()
+        .lines()
+        .enumerate()
+        .filter_map(|(i,x)| {
+            let trimmed_str = x.trim();
+            if String::from(trimmed_str).chars().collect::<Vec<char>>()[(i*3 as usize)%trimmed_str.len()] == '#' {
+                Some(String::from(x))
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<String>>().len();
 
+    Ok(trees.to_string())
+}
--- 2020/day3/part2/src/lib.rs
+++ 2020/day3/part2/src/lib.rs
@@ -1,7 +1,26 @@
-pub fn run(args: &Vec<String>) -> Result<String, &'static str> {
-    for arg in args {
-        println!("{}", arg);
-    }
-    Err("Not Implemented!")
-}
+const SLOPES: [(u32, u32); 5] = [(1,1),(3,1),(5,1),(7,1),(1,2)];
 
+pub fn run(args: &Vec<String>) -> Result<String, &'static str> {
+    let mut total_trees: u64 = 1;
+    for (run, rise) in SLOPES.iter() {
+        let trees = args[0]
+            .trim()
+            .lines()
+            .enumerate()
+            .filter_map(|(i,x)| {
+                if i as u32 % rise != 0 {
+                    return None
+                }                
+                let trimmed_str = x.trim();
+                if String::from(trimmed_str).chars().collect::<Vec<char>>()[((i as u32)/rise * run) as usize%trimmed_str.len()] == '#' {
+                    Some(String::from(x))
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<String>>().len();
+        total_trees *= trees as u64;
+    }
+
+    Ok(total_trees.to_string())
+}
