57ae5828dc02bd73030befd0f9e4d1ca0ec04e71
Sean Owens <seangabrielowens@gmail.com>
2022-12-03 00:48:52 -0500

    2022 Day 3 Solution

---
+++ 2022/day3.rb
@@ -0,0 +1,21 @@
+def part1(input)
+    input.map { |l| 
+        match = l.shift(l.count/2).intersection(l)[0] 
+        match > 90 ? match - 96 : match - 38
+    }.sum
+end
+
+def part2(input)
+    sum = 0
+    input.each_slice(3) do |x,y,z|
+        badge = (x & y & z)[0]
+        sum += badge - (badge > 90 ? 96 : 38)
+    end
+    return sum
+end
+
+actual = File.read('./input.txt').lines.map(&:chomp).map(&:codepoints)
+actual2 = File.read('./input.txt').lines.map(&:chomp).map(&:codepoints)
+
+puts part1(actual)
+puts part2(actual2)
