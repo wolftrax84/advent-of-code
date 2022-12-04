89068113ce254cce9c7c9ea7f08128a50132284b
Sean Owens <seangabrielowens@gmail.com>
2022-12-04 00:37:49 -0500

    2022 Day 4 Update

--- 2022/day4.rb
+++ 2022/day4.rb
@@ -1,19 +1,13 @@
 def part1(input)
     input.select{ |pair|
-        ranges = pair.split(',').map{|x| 
-            limits = x.split('-').map(&:to_i)
-            (limits[0]..limits[1]).to_a
-        }
+        ranges = pair.split(',').map{|x| x.split('-').map(&:to_i)}.map {(_1.._2).to_a}
         ranges.map{|r| r.count}.include?(ranges[0].intersection(ranges[1]).count)
     }.count
 end
 
 def part2(input)
     input.select {|pair|
-        ranges = pair.split(',').map{|x| 
-            limits = x.split('-').map(&:to_i)
-            (limits[0]..limits[1]).to_a
-        }
+        ranges = pair.split(',').map{|x| x.split('-').map(&:to_i)}.map {(_1.._2).to_a}
         ranges[0].intersection(ranges[1]).count > 0
     }.count
 end
